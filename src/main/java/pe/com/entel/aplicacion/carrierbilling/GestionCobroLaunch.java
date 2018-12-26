cda7c00c754313cdb6bcf97ec14ae85bfa7cc893
yorchejs <jorge.segovia@vfcons.com>
2018-12-26 16:49:31 -0500

    Update GestionCobroLaunch.java

--- src/main/java/pe/com/entel/aplicacion/carrierbilling/GestionCobroLaunch.java
+++ src/main/java/pe/com/entel/aplicacion/carrierbilling/GestionCobroLaunch.java
@@ -60,7 +60,7 @@ public class GestionCobroLaunch {
 
             TimeUnit.SECONDS.sleep(10);
 
-            logger.debug("Esperando 30 sec...");
+            logger.debug("Esperando 40 sec...");
 
             String jobName1 = "gestionCancelacionJob";
 
