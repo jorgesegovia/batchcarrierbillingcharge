95f8d45b09fca891463761e9e3e90cdc994698fa
Walter <Usuario@DESKTOP-4SRTAUK>
2019-01-08 17:18:35 -0500

    Quitando Log

--- src/main/java/pe/com/entel/aplicacion/carrierbilling/service/GestionCancelacionService.java
+++ src/main/java/pe/com/entel/aplicacion/carrierbilling/service/GestionCancelacionService.java
@@ -156,7 +156,6 @@ public class GestionCancelacionService {
 		header4.setNombre("requestTimestamp");
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
 		String fecha = dateFormat.format(new Date()) + "-05:00";
-		logger.debug(fecha);
 
 		header4.setValor(fecha);
 		listaHeaders.add(header4);
